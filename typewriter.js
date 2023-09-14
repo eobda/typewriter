4cac980efc3ec32620fa3eb08365945026503909
maddie <lee.maddie@gmail.com>
2023-09-14 10:53:01 -0400

    Added final newline character - still bugged!

--- typewriter.js
+++ typewriter.js
@@ -1,10 +1,15 @@
 const sentence = "hello there from lighthouse labs";
 let timer = 0;
 
-// Prints each character of sentence
 for (const char of sentence) {
+  // Prints each character of sentence
   setTimeout(() => {
     process.stdout.write(char);
   }, timer);
+  
+  // Timer increments to prepare for next character
   timer += 50;
 }
+
+// Writes a final newline character so next prompt starts on its own line
+process.stdout.write('\n');
